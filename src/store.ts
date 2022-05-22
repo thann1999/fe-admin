6d450633cb6ac3b193200472e3c8de1d7fbd9f3d
Thann <nguyenthanhthang185@gmail.com>
2022-05-22 20:52:01 +0700

    config redux persist

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,33 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import user from 'app/services/redux/slices/user-slice';
+import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import rootReducer from 'app/services/redux/root-reducer';
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Config Redux-Persist
+const persistConfig = {
+  storage,
+  key: 'user',
+  whiteList: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type AppDispatch = typeof store.dispatch;
